f7920b6a73cc2395e129a3f4bbedc5a5c25b1fda
Martin Kielhorn <wolpumba@gmail.com>
2023-11-29 00:06:34 +0100

    add more documentation

--- examples/06_camera2_grpc/app/src/main/java/com/example/camera2api_empty_views/MainActivity.kt
+++ examples/06_camera2_grpc/app/src/main/java/com/example/camera2api_empty_views/MainActivity.kt
@@ -14,6 +14,11 @@ import android.os.HandlerThread
 import android.view.Surface
 import android.view.TextureView
 
+
+// when activity starts, a grpc server is initialized using grpc-netty-shaded
+// and a camera is opened using camera2 api
+// the camera is displayed on the screen using a textureView
+
 class MainActivity : AppCompatActivity() {
     lateinit var handler: Handler
     private lateinit var handlerThread: HandlerThread
@@ -27,14 +32,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // getPermissions() is called to request permissions for camera and storage
         getPermissions()
 
+        // textureView is initialized and a surfaceTextureListener is set
         textureView = findViewById(R.id.textureView)
+        // cameraManager is initialized, it's task is to open the camera
         cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
+        // handlerThread and handler are initialized, they are used to open the camera
         handlerThread = HandlerThread("videoThread")
         handlerThread.start()
         handler = Handler(handlerThread.looper)
 
+        // surfaceTextureListener opens the camera when the textureView is available
+        // and deallocates the camera when the textureView is destroyed
         textureView.surfaceTextureListener =
             object: TextureView.SurfaceTextureListener{
                 override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
@@ -57,6 +69,7 @@ class MainActivity : AppCompatActivity() {
     fun openCamera() {
         cameraManager.openCamera(cameraManager.cameraIdList[0], object: CameraDevice.StateCallback(){
             override fun onOpened(p0: CameraDevice) {
+                // cameraDevice is initialized and a captureRequest is created
                 cameraDevice = p0
                 capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
                 val surface= Surface(textureView.surfaceTexture)
@@ -64,6 +77,7 @@ class MainActivity : AppCompatActivity() {
                 cameraDevice.createCaptureSession(listOf(surface),
                     object: CameraCaptureSession.StateCallback(){
                         override fun onConfigured(p0: CameraCaptureSession) {
+                            // cameraCaptureSession is initialized and a repeating request is set
                             cameraCaptureSession = p0
                             captureRequest = capReq.build()
                             cameraCaptureSession.setRepeatingRequest(captureRequest, null, null)
@@ -87,6 +101,7 @@ class MainActivity : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
+        // if permissions are not granted, getPermissions() is called again
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         grantResults.forEach {
             if(it != PackageManager.PERMISSION_GRANTED)
@@ -94,6 +109,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun getPermissions(){
+        // permissions for camera and storage are requested
         val permissionsLst = mutableListOf<String>()
         if(checkSelfPermission(android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
             permissionsLst.add(android.Manifest.permission.CAMERA)
@@ -104,6 +120,7 @@ class MainActivity : AppCompatActivity() {
         if(checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
             permissionsLst.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
         }
+        // if permissionsLst is not empty, permissions are requested
         if (permissionsLst.size > 0){
             requestPermissions(permissionsLst.toTypedArray(),101)
         }
